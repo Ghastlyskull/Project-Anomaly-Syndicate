e184f3d77a3e2f5e7b99ce40c76e62f4a8f7308b
Nalzurin <alexnormandi@ukr.net>
2024-11-06 02:33:46 +0200

    Add delay and stun to devourer vomiting

--- Sources/ProjectAnomalySyndicate/ProjectAnomalySyndicate/HarmonyPatches/DevourerAbortDigestionSpitbanePatch.cs
+++ Sources/ProjectAnomalySyndicate/ProjectAnomalySyndicate/HarmonyPatches/DevourerAbortDigestionSpitbanePatch.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using UnityEngine;
 using Verse;
 
 namespace ProjectAnomalySyndicate.HarmonyPatches
@@ -13,16 +14,19 @@ namespace ProjectAnomalySyndicate.HarmonyPatches
     [HarmonyPatch]
     public static class DevourerAbortDigestionSpitbanePatch
     {
+        
         private static MethodBase TargetMethod()
         {
             return AccessTools.Method(typeof(CompDevourer), nameof(CompDevourer.CompTick));
         }
 
-        private static void Postfix(CompDevourer __instance)
+        private static void Postfix(CompDevourer __instance, int ___ticksDigesting)
         {
-            if (__instance.Digesting && __instance.DigestingPawn.health.hediffSet.HasHediff(DefOfs.Spitbane))
+
+            if (__instance.Digesting && __instance.DigestingPawn.health.hediffSet.HasHediff(DefOfs.Spitbane) && ___ticksDigesting == 80)
             {
                 __instance.DigestJobFinished();
+                __instance.Pawn.stances?.stunner?.StunFor(160, null);
             }
         }
     }
