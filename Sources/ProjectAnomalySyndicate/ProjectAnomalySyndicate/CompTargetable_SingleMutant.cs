1611eb8b30d18328ca2814baf7fbb74d4ca21839
Nalzurin <alexnormandi@ukr.net>
2024-11-08 08:39:06 +0200

    Implement a way for ghouls to act like pawns

---
+++ Sources/ProjectAnomalySyndicate/ProjectAnomalySyndicate/CompTargetable_SingleMutant.cs
@@ -0,0 +1,29 @@
+﻿using RimWorld;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Verse;
+
+namespace ProjectAnomalySyndicate
+{
+    public class CompTargetable_SingleMutant : CompTargetable
+    {
+        protected override bool PlayerChoosesTarget => true;
+
+        protected override TargetingParameters GetTargetingParameters()
+        {
+            return new TargetingParameters
+            {
+                canTargetMutants = true,
+                canTargetBuildings = false
+            };
+        }
+
+        public override IEnumerable<Thing> GetTargets(Thing targetChosenByPlayer = null)
+        {
+            yield return targetChosenByPlayer;
+        }
+    }
+}
