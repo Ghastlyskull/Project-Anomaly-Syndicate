871f0263cc27f7bda2215f81a1730d817f14e90f
Nalzurin <alexnormandi@ukr.net>
2024-11-13 11:17:53 +0200

    Fix null errors for quest, add anesthetic

--- Sources/ProjectAnomalySyndicate/ProjectAnomalySyndicate/SyndicateUtility.cs
+++ Sources/ProjectAnomalySyndicate/ProjectAnomalySyndicate/SyndicateUtility.cs
@@ -22,7 +22,6 @@ namespace ProjectAnomalySyndicate
                 list.Add(PawnKindDefOf.Chimera);
                 list.Add(DefOfs.Devourer);
                 list.Add(PawnKindDefOf.Sightstealer);
-                list.Add(PawnKindDefOf.Ghoul);
             }
             return list;
         }
